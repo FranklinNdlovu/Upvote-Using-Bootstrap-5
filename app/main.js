38dc15fb4c72471dbccdfce0529eecfa2dc49c72
Franklin Ndlovu <franklin.ndlovu@gmail.com>
2022-07-18 21:55:36 +0200

    added a submission component to separate logic

--- app/main.js
+++ app/main.js
@@ -1,21 +1,61 @@
+const submissionComponent = {
+    template: `
+            <div class="d-flex align-items-centre">
+                <div class="flex-shrink-0">
+                    <img class="image is-64x64" v-bind:src="submission.submissionImage"
+                        alt="Sample Image">
+                </div>
+                <div class="flex-grow-1 ms-3">
+                    <h5>
+                        <a v-bind:href="submission.url" class="text-primary">{{submission.title}}</a>
+                        <small class="tag text-muted">#{{submission.id}}</small>
+                    </h5>
+                    <p>
+                        {{submission.description}}
+                        <br />
+                    <div class="d-flex">
+                        <small class="is-size-7 align-self-center">
+                            Submitted by:
+                        </small>
+                        <img class=" ms-3 rounded-circle image is-24x24 align-self-center"
+                            v-bind:src="submission.avatar" alt="">
+                    </div>
+                    </p>
+                </div>
+                <div class="flex-shrink-0">
+                    <span class="icon is-small ">
+                        <i class="row fa fa-chevron-up" v-on:click="upvote(submission.id)"></i>
+                        <strong class="row text-primary">{{submission.votes}}</strong>
+                        <i class="row fa fa-chevron-down" v-on:click="downvote(submission.id)"></i>
+                    </span>
+                </div>
+            </div>
+    `,
+    props: ['submission', 'submissions'],
+    methods: {
+        upvote(id) {
+            this.submissions.find(x => x.id == id).votes += 1;
+        },
+        downvote(id) {
+            this.submissions.find(x => x.id == id).votes -= 1;
+        }
+    },
+};
+
 const upvoteApp = {
     data() {
         return {
             submissions: Seed.submissions
         }
     },
-    computed:{
-        sortedSubmissions(){
-            return this.submissions.sort((a,b) => b.votes - a.votes);
+    computed: {
+        sortedSubmissions() {
+            return this.submissions.sort((a, b) => b.votes - a.votes);
         }
     },
-    methods: {
-        upvote(id){
-            this.submissions.find(x => x.id==id).votes+=1;
-        },
-        downvote(id){
-            this.submissions.find(x => x.id==id).votes-=1;
-        }
+    
+    components:{
+        "submission-component": submissionComponent
     }
 };
 
